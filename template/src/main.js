e58edea9f3df27f94b67b1a0ba53df41aafb8c36
gonghao <gonghao1@jd.com>
2018-01-14 00:41:58 +0800

    添加过滤器

--- template/src/main.js
+++ template/src/main.js
@@ -2,26 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // import 'es6-promise/auto';
 import Vue from 'vue';
+import attachFastClick from 'fastclick';
 import App from './App';
 import router from './router';
 import store from './store';
 import './assets/css/common.scss';
 import Confirm from './plugins/confirm/index';
 import Loading from './plugins/loading/index';
-// import 'core-js/fn/array/includes';
+import filters from './filters';
 
 if (!window.Promise) {
   window.Promise = Promise;
 }
-
-const attachFastClick = require('fastclick');
-
 attachFastClick.attach(document.body);
 
 Vue.config.productionTip = false;
 
 Vue.use(Confirm);
 Vue.use(Loading);
+// 注册过滤器
+Object.keys(filters).forEach(filter => Vue.filter(filter, filters[filter]));
 
 /* eslint-disable no-new */
 new Vue({
